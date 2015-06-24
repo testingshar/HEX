6de0f2d3a8132a69000d2ce483ec1d2093a72123
testingshar <testshar@yandex.ru>
2015-06-25 02:32:15 +0400

    очередная позорная лажа

--- pages/students/ivanov.php
+++ pages/students/ivanov.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Документ без названия</title>
-  <link href="../../css/style.css" type="text/css" rel="stylesheet">
-</head>
-<body>
-<div class="wrapper">
-  <?php include $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header.html'; ?>
-  <main>
-    <br><br><br><br>
-    <img src="../../images/students/ivanov.jpg">
-    <h1>Студент Иванов</h1>
-    <br>
-    Характер трусливый. Троешник, постоянно на измене. Вид придурковато-глуповатый.
-  </main>
-  <?php include $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer.html'; ?>
-</div>
-</body>
-</html>
+<?php include $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header.html'; ?>
+<main>
+  <br><br><br><br>
+  <img src="../../images/students/ivanov.jpg">
+  <h1>Студент Иванов</h1>
+  <br>
+  Характер трусливый. Троешник, постоянно на измене. Вид придурковато-глуповатый.
+</main>
+<?php include $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer.html'; ?>
