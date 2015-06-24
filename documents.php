b352cacfb0b44cb49eee0b9cad94c32ddb70833f
testingshar <testshar@yandex.ru>
2015-06-25 02:18:23 +0400

    позорная лажа

--- documents.php
+++ documents.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Страница документов</title>
-  <link href="../css/style.css" type="text/css" rel="stylesheet">
-</head>
-<body>
 <?php include $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header.html'; ?>
-<div class="wrapper">
-  <main>
 <h1>Данная страница содержит перечень документов</h1>
     <ul>
       <li>Общие положения. Терминология. Документация</li>
@@ -21,8 +11,4 @@
       <li>Механические системы</li>
       <li>Гидравлические и пневматические системы</li>
     </ul>
-  </main>
-  <?php include $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer.html'; ?>
-</div>
-</body>
-</html>
+<?php include $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer.html'; ?>
---
+++ students.php
@@ -0,0 +1,12 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'header.html'; ?>    
+  <h1>Наши студенты</h1>
+  <br><br><br>
+  <a href="pages/students/ivanov.html"><img src="../../images/students/ivanov.jpg"></a>
+  <figcaption>Студент Иванов</figcaption>
+  <br>
+  <a href="pages/students/pupkin.html"><img src="../../images/students/pupkin.jpg"></a>
+  <figcaption>Студент Пупкин</figcaption>
+  <br>
+  <img src="../../images/students/billi.jpg">
+  <figcaption>Студент Гейтс</figcaption>
+<?php include $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'footer.html'; ?>
